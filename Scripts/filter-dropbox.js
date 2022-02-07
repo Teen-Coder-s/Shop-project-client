c9210ce9d4a0e3d96d2cc7402b1651b0d940fb89
abdoki7788 <abdoki7788@gmail.com>
2022-02-07 10:39:38 +0330

    fixed bugs

--- Scripts/filter-dropbox.js
+++ Scripts/filter-dropbox.js
@@ -1,10 +1,18 @@
-let dropdownsHeader = document.querySelectorAll('.filter_header')
-let dropdownsBody = document.querySelectorAll('.filter_body')
-let dropdownsIcon = document.querySelectorAll('.filter_dropdown_icon')
-dropdownsHeader.forEach((el, ind) => {
+let dropdownsHeaders = document.querySelectorAll('.filter_header')
+let dropdownsBodys = document.querySelectorAll('.filter_body')
+let dropdownsIcons = document.querySelectorAll('.filter_dropdown_icon')
+let toggleButtons = document.querySelectorAll('.toggle-button')
+let toggleCheckboxes = document.querySelectorAll('.toggle-checkbox')
+dropdownsHeaders.forEach((el, ind) => {
 	el.addEventListener('click', () => {
-		dropdownsBody[ind].classList.toggle('filter_hidden')
-		dropdownsHeader[ind].classList.toggle('filter_header-border')
-		dropdownsIcon[ind].classList.toggle('filter_dropdown_icon_transform')
+		dropdownsBodys[ind].classList.toggle('filter_hidden')
+		dropdownsHeaders[ind].classList.toggle('filter_header-border')
+		dropdownsIcons[ind].classList.toggle('filter_dropdown_icon_transform')
+	})
+})
+
+toggleButtons.forEach((el, ind) => {
+	el.addEventListener('click', () => {
+		toggleCheckboxes[ind].checked = !toggleCheckboxes[ind].checked
 	})
 })
